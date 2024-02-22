0f1227ebb2eb41936517134acf9a6d26f89ac58c
Urs Schmidt <urs.schmidt@ts.berlin>
2024-02-22 16:14:51 +0100

    chores: add configurations

--- portal/amarex/config.js
+++ portal/amarex/config.js
@@ -4,7 +4,7 @@ const Config = {
         fetchBroadcastUrl: "./resources/newsFeedPortalAlerts.json"
     },
     namedProjections: [
-        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+        ["EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
     ],
     layerConf: "./resources/services-internet.json",
     restConf: "./resources/rest-services-internet.json",
