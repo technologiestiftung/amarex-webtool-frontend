7a92bea43faefe05acc1340d0ed11580ef6d56b7
Urs Schmidt <urs.schmidt@ts.berlin>
2024-02-22 09:44:03 +0100

    chores: install lint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["vue"],
+    rules: {
+        "no-console": "off",
+        "no-unused-vars": "off",
+        "no-undef": "off",
+    },
+};
+
