d2ce193655106d66701300a861d9a4769ab9c5ae
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2024-03-12 11:42:56 +0100

    feat: change style for abimo layer

--- src_3_0_0/modules/abimo/components/AbimoMeasure.vue
+++ src_3_0_0/modules/abimo/components/AbimoMeasure.vue
@@ -1,48 +1,45 @@
 <script>
-import {mapActions, mapGetters} from "vuex";
+import { mapActions, mapGetters } from "vuex";
 import Feature from "ol/Feature";
 import Style from "ol/style/Style";
 import Fill from "ol/style/Fill";
 import Stroke from "ol/style/Stroke";
-import {Select} from "ol/interaction";
-import {dblclick, never} from "ol/events/condition";
+import { Select } from "ol/interaction";
+import { dblclick, never } from "ol/events/condition";
 /**
  * Abimo
  * @module modules/AbimoMeasure
  */
 export default {
     name: "AbimoMeasure",
-    data () {
+    data() {
         return {
             features: [],
             sliders: [
                 {
                     id: "slider1",
-                    label: "Regler 1",
+                    label: "Versickerungsmulde",
                     value: 0,
-                    info: "Info zu Regler 1"
                 },
                 {
                     id: "slider2",
-                    label: "Regler 2",
+                    label: "Gründach",
                     value: 0,
-                    info: "Info zu Regler 2"
                 },
                 {
                     id: "slider3",
-                    label: "Regler 3",
+                    label: "Rigolen",
                     value: 0,
-                    info: "Info zu Regler 3"
-                }
+                },
             ],
             selectInteraction: null,
-            layer: null
+            layer: null,
         };
     },
     computed: {
-        ...mapGetters(["configJs"])
+        ...mapGetters(["configJs"]),
     },
-    mounted () {
+    mounted() {
         this.createInteractions();
         this.layer = mapCollection
             .getMap("2D")
@@ -53,7 +50,7 @@ export default {
     methods: {
         ...mapActions("Maps", {
             addInteractionToMap: "addInteraction",
-            removeInteractionFromMap: "removeInteraction"
+            removeInteractionFromMap: "removeInteraction",
         }),
         /**
          * Creates the interactions for selecting features.
@@ -66,7 +63,7 @@ export default {
                 multi: true,
                 layers: function (layer) {
                     return layer.get("id") === "basis_abimo";
-                }
+                },
             });
 
             this.selectInteraction = selectInteraction;
@@ -83,54 +80,48 @@ export default {
             });
             this.addInteractionToMap(selectInteraction);
         },
-        measuresToRGB (measure1, measure2, measure3) {
+        measuresToRGB(measure1, measure2, measure3) {
             const red = Math.round((measure1 / 100) * 255),
                 green = Math.round((measure2 / 100) * 255),
                 blue = Math.round((measure3 / 100) * 255);
 
             return `rgb(${red},${green},${blue})`;
         },
-        createStyle (properties) {
+        createStyle(properties) {
             const rules = [
-                    {min: 0, max: 30, color: [108, 245, 66, 1]},
-                    {min: 31, max: 60, color: [115, 237, 66, 1]},
-                    {min: 61, max: 90, color: [122, 230, 66, 1]},
-                    {min: 91, max: 120, color: [129, 222, 66, 1]},
-                    {min: 121, max: 150, color: [135, 215, 66, 1]},
-                    {min: 151, max: 180, color: [142, 207, 66, 1]},
-                    {min: 181, max: 210, color: [149, 199, 66, 1]},
-                    {min: 211, max: 240, color: [156, 192, 66, 1]},
-                    {min: 241, max: 270, color: [163, 184, 66, 1]},
-                    {min: 271, max: 300, color: [170, 177, 66, 1]},
-                    {min: 301, max: 330, color: [177, 169, 66, 1]},
-                    {min: 331, max: 360, color: [183, 161, 66, 1]},
-                    {min: 361, max: 390, color: [190, 154, 66, 1]},
-                    {min: 391, max: 420, color: [197, 146, 66, 1]},
-                    {min: 421, max: 450, color: [204, 139, 66, 1]},
-                    {min: 451, max: 480, color: [211, 131, 66, 1]},
-                    {min: 481, max: 510, color: [218, 123, 66, 1]},
-                    {min: 511, max: 540, color: [224, 116, 66, 1]},
-                    {min: 541, max: 570, color: [231, 108, 66, 1]},
-                    {min: 571, max: 600, color: [238, 101, 66, 1]}
+                    { min: 0, max: 1, color: [108, 245, 66, 1] },
+                    { min: 1, max: 50, color: [115, 237, 66, 1] },
+                    { min: 50, max: 100, color: [122, 230, 66, 1] },
+                    { min: 100, max: 150, color: [129, 222, 66, 1] },
+                    { min: 150, max: 200, color: [135, 215, 66, 1] },
+                    { min: 200, max: 250, color: [142, 207, 66, 1] },
+                    { min: 250, max: 300, color: [149, 199, 66, 1] },
+                    { min: 300, max: 350, color: [156, 192, 66, 1] },
+                    { min: 350, max: 400, color: [163, 184, 66, 1] }
                 ],
-                matchingRule = rules.find(rule => properties.r >= rule.min && properties.r <= rule.max),
-                fillColor = matchingRule ? matchingRule.color : [255, 255, 255, 1];
+                matchingRule = rules.find(
+                    (rule) =>
+                        properties.row >= rule.min && properties.row <= rule.max
+                ),
+                fillColor = matchingRule
+                    ? matchingRule.color
+                    : [255, 14, 14, 1];
 
             return new Style({
                 stroke: new Stroke({
-                    color: this.measuresToRGB(properties.measure1, properties.measure2, properties.measure3),
-                    width: 2
+                    color: this.measuresToRGB(
+                        properties.measure1,
+                        properties.measure2,
+                        properties.measure3
+                    ),
+                    width: 2,
                 }),
                 fill: new Fill({
-                    color: fillColor
-                })
+                    color: fillColor,
+                }),
             });
         },
-        showInfo (info) {
-            // eslint-disable-next-line no-alert
-            alert(info);
-        },
-        mapToOlFeatures (featuresArray) {
+        mapToOlFeatures(featuresArray) {
             const sliderValues = this.sliders.map((slider) => slider.value);
 
             return featuresArray.map((featureData) => {
@@ -145,18 +136,18 @@ export default {
                     regenja: featureData.values_.regenja,
                     measure1: sliderValues[0],
                     measure2: sliderValues[1],
-                    measure3: sliderValues[2]
+                    measure3: sliderValues[2],
                 });
 
                 return olFeature;
             });
         },
-        addToLayer (features) {
+        addToLayer(features) {
             const layerSource = this.layer.values_.source;
 
             layerSource.addFeatures(features);
         },
-        logFunctionsAndProperties (obj) {
+        logFunctionsAndProperties(obj) {
             const methods = Object.getOwnPropertyNames(obj).filter(
                     (key) => typeof obj[key] === "function"
                 ),
@@ -168,59 +159,71 @@ export default {
             console.warn(properties);
             console.warn(JSON.stringify(obj));
         },
-        async applyMeasures () {
-            if (this.features.length) {
-                const olFeatures = this.mapToOlFeatures(this.features);
+        async applyMeasures() {
+            const olFeatures = this.mapToOlFeatures(this.features);
 
-                for (const feature of olFeatures) {
-                    const properties = feature.getProperties(),
-                        {schl5, r, verdunstun, sonstigeWerte, row, ri, regenja, measure1, measure2, measure3} = properties,
-                        payload = {schl5, r, verdunstun, sonstigeWerte, row, ri, regenja, measure1, measure2, measure3};
+            for (const feature of olFeatures) {
+                const properties = feature.getProperties(),
+                    {
+                        schl5,
+                        r,
+                        verdunstun,
+                        sonstigeWerte,
+                        row,
+                        ri,
+                        regenja,
+                        measure1,
+                        measure2,
+                        measure3,
+                    } = properties,
+                    payload = {
+                        schl5,
+                        r,
+                        verdunstun,
+                        sonstigeWerte,
+                        row,
+                        ri,
+                        regenja,
+                        measure1,
+                        measure2,
+                        measure3,
+                    };
 
-                    try {
-                        const response = await fetch("http://localhost:3000/calculate", {
+                try {
+                    const response = await fetch(
+                        "http://localhost:3000/calculate",
+                        {
                             method: "POST",
                             headers: {
-                                "Content-Type": "application/json"
+                                "Content-Type": "application/json",
                             },
-                            body: JSON.stringify(payload)
-                        });
-
-                        if (!response.ok) {
-                            throw new Error("Netzwerkantwort war nicht ok.");
+                            body: JSON.stringify(payload),
                         }
+                    );
 
-                        // eslint-disable-next-line one-var
-                        const updatedProperties = await response.json();
-
-                        feature.setProperties(updatedProperties);
-                        feature.setStyle(this.createStyle(updatedProperties));
-
+                    if (!response.ok) {
+                        throw new Error("Netzwerkantwort war nicht ok.");
                     }
-                    catch (error) {
-                        console.error("Fehler beim Abrufen der Daten:", error);
-                    }
+
+                    // eslint-disable-next-line one-var
+                    const updatedProperties = await response.json();
+
+                    feature.setProperties(updatedProperties);
+                    feature.setStyle(this.createStyle(updatedProperties));
+                } catch (error) {
+                    console.error("Fehler beim Abrufen der Daten:", error);
                 }
-
-
-                this.addToLayer(olFeatures);
             }
-            else {
-                console.warn(
-                    "Keine Features ausgewählt, Maßnahmen können nicht angewendet werden."
-                );
-            }
-        }
-    }
+
+            this.addToLayer(olFeatures);
+        },
+    },
 };
 </script>
 
 <template lang="html">
     <div id="abimo">
-        <div
-            v-for="slider in sliders"
-            :key="slider.id"
-        >
+        <div v-for="slider in sliders" :key="slider.id">
             <label :for="slider.id">{{ slider.label }}</label>
             <input
                 :id="slider.id"
@@ -228,23 +231,14 @@ export default {
                 type="range"
                 min="0"
                 max="100"
-            >
+            />
             <span>{{ slider.value }}</span>
-            <button @click="showInfo(slider.info)">
-                i
-            </button>
         </div>
-        <button
-            :disabled="!features.length"
-            @click="applyMeasures"
-        >
+        <button :disabled="!features.length" @click="applyMeasures">
             Maßnahmen anwenden
         </button>
         <ul>
-            <li
-                v-for="feature in features"
-                :key="feature.code"
-            >
+            <li v-for="feature in features" :key="feature.code">
                 Code: {{ feature.values_.schl5 }}, R: {{ feature.values_.r }}
             </li>
         </ul>
