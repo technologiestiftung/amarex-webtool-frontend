9343b50b8810bd419f7c58f3904ef53922759707
Urs Schmidt <urs.schmidt@ts.berlin>
2024-05-13 14:54:40 +0200

    feat: abimo WMF setup

--- devtools/webpack.dev.js
+++ devtools/webpack.dev.js
@@ -9,15 +9,14 @@ const merge = require("webpack-merge"),
     /* eslint-disable no-process-env */
     proxyServer = process.env.HTTPS_PROXY || process.env.HTTP_PROXY,
     /* eslint-disable no-process-env */
-    proxyAgent = proxyServer !== undefined ? new HttpsProxyAgent(proxyServer) : "";
-
+    proxyAgent =
+        proxyServer !== undefined ? new HttpsProxyAgent(proxyServer) : "";
 
 let proxies;
 
 if (fse.existsSync("./devtools/proxyconf.json")) {
     proxies = require("./proxyconf.json");
-}
-else {
+} else {
     proxies = require("./proxyconf_example.json");
 }
 if (fse.existsSync("./devtools/securedServices.js")) {
@@ -29,53 +28,59 @@ if (fse.existsSync("./devtools/securedServices.js")) {
     proxies["/secure_"] = securedServices.pathRewrite(securedProxy);
 }
 
-Object.keys(proxies).forEach(proxy => {
+Object.keys(proxies).forEach((proxy) => {
     if (proxies[proxy].agent !== undefined) {
         proxies[proxy].agent = proxyAgent;
     }
 });
 
 module.exports = function () {
-    return merge.smart({
-        mode: "development",
-        devtool: "cheap-module-eval-source-map",
-        devServer: {
-            headers: {
-                "Access-Control-Allow-Origin": "*"
-            },
-            https: true,
-            open: false,
-            openPage: "portal/master",
-            overlay: true,
-            port: 9001,
-            proxy: proxies,
-            publicPath: "/build/"
-        },
-        module: {
-            rules: [
-                // Bootstrap Icons werden von bootstrap gelesen
-                {
-                    test: /bootstrap-icons\.(eot|svg|ttf|woff|woff2)$/,
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        publicPath: "../../node_modules/bootstrap-icons/font/fonts"
-                    }
+    return merge.smart(
+        {
+            mode: "development",
+            devtool: "cheap-module-eval-source-map",
+            devServer: {
+                headers: {
+                    "Access-Control-Allow-Origin":
+                        "https://technologiestiftung-berlin.webgis1.com",
                 },
-                // alle anderen Schriftarten
-                {
-                    test: /\.(eot|svg|ttf|woff|woff2)$/,
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        publicPath: "../../src_3_0_0/assets/css/fonts"
-                    }
-                }
-            ]
-        }
-        // comment in to create a graphical representation of the bundle as html that is automatically displayed in the browser at 'npm run start'
-        // ,plugins: [
-        //     new BundleAnalyzerPlugin()
-        // ]
-    }, new Common());
+                https: true,
+                open: false,
+                openPage: "portal/amarex",
+                overlay: true,
+                port: 9001,
+                proxy: proxies,
+                publicPath: "/build/",
+            },
+            module: {
+                rules: [
+                    // Bootstrap Icons werden von bootstrap gelesen
+                    {
+                        test: /bootstrap-icons\.(eot|svg|ttf|woff|woff2)$/,
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            publicPath:
+                                "../../node_modules/bootstrap-icons/font/fonts",
+                        },
+                    },
+                    // alle anderen Schriftarten
+                    {
+                        test: /\.(eot|svg|ttf|woff|woff2)$/,
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            publicPath: "../../src_3_0_0/assets/css/fonts",
+                        },
+                    },
+                ],
+            },
+            // comment in to create a graphical representation of the bundle as html that is automatically displayed in the browser at 'npm run start'
+            // ,plugins: [
+            //     new BundleAnalyzerPlugin()
+            // ]
+        },
+        new Common()
+    );
 };
+
