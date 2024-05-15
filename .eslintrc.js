dc503bce67f3ce77ad45ac45df5aae3fbbb17a78
Urs Schmidt <urs.schmidt@ts.berlin>
2024-05-15 15:25:25 +0200

    feat: add prettier + configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
+    extends: ["eslint:recommended", "plugin:vue/vue3-essential","prettier"],
     overrides: [
         {
             env: {
