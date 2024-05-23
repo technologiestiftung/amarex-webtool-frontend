83b1aa57b6ceb935043fb73b32a4234b67ad1c52
Luise Brandenburger <luise.brandenburger@ts.berlin>
2024-05-23 13:28:54 +0200

    feat: add constants

---
+++ addons/addons_3_0_0/exporterAddon/constants/exportFormats.js
@@ -0,0 +1,9 @@
+const exportFormats = {
+  geoJson: "geojson",
+  gml: "gml",
+  shp: "shp",
+  gpkg: "gpkg",
+};
+
+export default exportFormats;
+
---
+++ addons/addons_3_0_0/exporterAddon/constants/layertypes.js
@@ -0,0 +1,8 @@
+const layertypes = {
+  wfs: "WFS",
+  draw: "DRAW",
+  geoJson: "GeoJSON",
+};
+
+export default layertypes;
+
