994e959acf9d3ab267ce90db45a795c67f9e6c8f
Luise Brandenburger <luise.brandenburger@ts.berlin>
2024-05-30 11:25:17 +0200

    feat: add config download

--- addons/addons_3_0_0/exporterAddon/components/ExporterAddon.vue
+++ addons/addons_3_0_0/exporterAddon/components/ExporterAddon.vue
@@ -1,4 +1,7 @@
 <script>
+import { mapGetters } from "vuex";
+import JSZip from "jszip";
+
 /**
  * ExporterAddon
  * @description Exporter Addon
@@ -8,9 +11,66 @@ export default {
   name: "ExporterAddon",
   data() {
     return {
-      fileToExport: false,
-      configToExport: {},
+      configToExport: null,
+      fileSources: [],
     };
+  },
+  computed: {
+    ...mapGetters(["allLayerConfigs"]),
+  },
+  methods: {
+    async prepareConfigForDownload() {
+      const layers = await this.allLayerConfigs;
+      const layerConfig = {
+        baselayer: { elements: [] },
+        subjectlayer: { elements: [] },
+      };
+
+      layers.forEach((layer) => {
+        const layerData = {
+          id: layer.id,
+          visibility: layer.visibility,
+        };
+
+        if (layer.baselayer) {
+          layerConfig.baselayer.elements.push(layerData);
+        } else {
+          layerConfig.subjectlayer.elements.push(layerData);
+        }
+      });
+
+      this.configToExport = { layerConfig };
+    },
+
+    forceFileDownload(zip, zipName) {
+      zip
+        .generateAsync({ type: "blob" })
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", `${zipName}.zip`);
+          document.body.appendChild(link);
+          link.click();
+          window.URL.revokeObjectURL(url);
+        })
+        .catch(() => console.log("error occured"));
+    },
+    async downloadWithFetch(files, zipName) {
+      const zip = new JSZip();
+      const fetchPromises = files.map(async (file) => {
+        const response = await fetch(file.src);
+        const buffer = await response.arrayBuffer();
+        zip.file(file.title, buffer);
+      });
+
+      // create config.json
+      const configJson = JSON.stringify(this.configToExport);
+      zip.file("config.json", configJson);
+
+      await Promise.all(fetchPromises);
+      this.forceFileDownload(zip, zipName);
+    },
   },
 };
 </script>
@@ -21,6 +81,16 @@ export default {
     class="ExporterAddon-root"
   >
     Exporter Addon
+
+    <div class="d-flex flex-column gap-3">
+      <button @click="prepareConfigForDownload()">
+        Get Config to Download
+      </button>
+
+      <button @click="downloadWithFetch(fileSources, 'amarex-download')">
+        Download ZIP
+      </button>
+    </div>
   </div>
 </template>
 
